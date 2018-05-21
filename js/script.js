ac103977bd095423f0887ea934f9fa21a00a4cf9
Piotr Strzelecki <strzelecki.piotras@gmail.com>
2018-05-21 22:05:12 +0200

    Add responsive navbar

--- js/script.js
+++ js/script.js
@@ -2,4 +2,5 @@ const navToggle = document.querySelector('.menu-toggle');
 
 navToggle.addEventListener('click', () => {
     document.body.classList.toggle('nav-open');
+    navToggle.classList.toggle('open');
 });
